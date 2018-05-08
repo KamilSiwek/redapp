821a78a5da9680feb84b9bd97cab7b4d6a3215cd
KamilSiwek <kontokamilsiwek@gmail.com>
2018-05-08 16:34:16 +0200

    add log

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('users/', include('users.urls')),
     path('posts/', include('posts.urls')),
+    path('', views.index, name="index"),
 ]
